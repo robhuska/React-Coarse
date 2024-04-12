c3379e98a16c8c370763ab0de3de7f7123562a4b
Rob Huska <rob.huska85@gmail.com>
2024-04-11 22:45:16 -0400

    Lesson 15

---
+++ 15-http-requests/src/components/AvailablePlaces.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import Error from './Error.jsx';
+import Places from './Places.jsx';
+import { useEffect } from 'react';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch('http://localhost:3000/placess');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch places.');
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || 'Could not find places, please try again later.',
+        });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+
+    // fetch('http://localhost:3000/places')
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((resData) => {
+    //     setAvailablePlaces(resData.places);
+    //   });
+  }, []);
+
+  if (error) {
+    return <Error title="An error occurred" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      loadingText="Fetching place data..."
+      isLoading={isFetching}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
