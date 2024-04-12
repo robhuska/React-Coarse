39c6814f0d2584692f22862fa61d06e3837a276b
Rob Huska <rob.huska85@gmail.com>
2024-04-11 22:24:57 -0400

    Lesson 4

---
+++ 04-essentials-deep-dive/src/components/Player.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export default function Player({
+  symbol,
+  initialName,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            value={playerName}
+            onChange={handleChange}
+            required
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+}
