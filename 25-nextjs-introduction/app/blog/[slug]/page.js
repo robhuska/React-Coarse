f46075abfa9463556d73d04ca66951b1da557c0b
Rob Huska <rob.huska85@gmail.com>
2025-02-18 21:46:26 -0500

    quick enhancement for intro to next

--- 25-nextjs-introduction/app/blog/[slug]/page.js
+++ 25-nextjs-introduction/app/blog/[slug]/page.js
@@ -1,7 +1,13 @@
-export default function BlogPost() {
+import Link from 'next/link';
+
+export default function BlogPost({ params }) {
   return (
     <main>
       <h1>Blog Post</h1>
+      <p>{params.slug}</p>
+      <p>
+        <Link href="../blog">Blog</Link>
+      </p>
     </main>
   );
 }
