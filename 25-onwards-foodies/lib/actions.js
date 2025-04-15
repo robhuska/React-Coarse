1be306587908acd185bf93d1240ebf37507f6471
Rob Huska <rob.huska85@gmail.com>
2025-04-15 15:59:55 -0400

    Lesson 25: error handeling for form

--- 25-onwards-foodies/lib/actions.js
+++ 25-onwards-foodies/lib/actions.js
@@ -2,7 +2,12 @@
 
 import { saveMeal } from './meals';
 import { redirect } from 'next/navigation';
-export async function shareMeal(formData) {
+
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
@@ -12,7 +17,21 @@ export async function shareMeal(formData) {
     creator_email: formData.get('email'),
   };
 
-  await saveMeal(meal);
+  if(
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: 'Invalid input.',
+    };
+  }
 
+  await saveMeal(meal);
   redirect('/meals');
 }
